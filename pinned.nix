8e4d6a3065c7d7526942a70fd4363c27aef56156
vroad <396351+vroad@users.noreply.github.com>
2021-04-01 13:07:51 +0900

    Pin packages

--- pinned.nix
+++ pinned.nix
@@ -1,6 +1,6 @@
-{ macos_sdk ? null }:
 let
   sources = import ./nix/sources.nix;
   nixpkgs = import sources.nixpkgs { };
+  nixcrpkgs = import ./default.nix { inherit nixpkgs; };
 in
-import ./top.nix { inherit nixpkgs macos_sdk; }
+nixcrpkgs
